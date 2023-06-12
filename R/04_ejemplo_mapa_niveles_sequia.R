ab4cf275fbc1752b3e0ac2a40d03a120f40ea441
Joshua Kunst <jbkunst@gmail.com>
2023-06-12 01:57:14 -0400

    Update 04_ejemplo_mapa_niveles_sequia.R

--- R/04_ejemplo_mapa_niveles_sequia.R
+++ R/04_ejemplo_mapa_niveles_sequia.R
@@ -11,36 +11,53 @@ d <- data_clima_sequia |>
 d2 <- left_join(data_geo,d ,by = c('cut_com' = 'code'))
 
 genePal <- function(vals,rev = TRUE){
+
   palette <- c('#730000','#E60000','#FFAA00','#FFD37F','#FFFF00','#FFFFFF','#8CCDEF','#00BFFF','#1D90FF','#4169E1','#0000FF')
+
   perc <- c(0,.02,.05,.1,.2,.3,.7,.8,.9,.95,.98,1)
+
   labels <-  c("Sequía excepcional", "Sequía extrema", "Sequía severa", "Sequía moderada", "Anormalmente seco",'Normal',
              "Anormalmente humedo",'Moderadamente humedo','Severamente humedo','Extramademente humedo', 'Excepcionalmente humedo')
 
   vals_map <- quantile(vals,perc,na.rm = TRUE) |> unique()
+
   vals_cut <- cut(vals,vals_map,include.lowest = TRUE)
+
   n <- levels(vals_cut) |> length()
+
   levels(vals_cut) <- if(rev) rev(labels[1:n]) else labels[1:n]
 
   pal <- colorFactor(
     palette = if (rev) rev(palette[1:n]) else palette[1:n],
-    domain = vals_cut)
+    domain = vals_cut
+    )
+
   return(list(pal,vals_cut))
 }
 
-v <- genePal(d2$spi_12,rev = FALSE)
+v <- genePal(vals = d2$spi_12, rev = FALSE)
 pal <- v[[1]]
+
 
 leaflet() |>
   addProviderTiles(leaflet::providers$OpenStreetMap.Mapnik) |>
-  addPolygons(data = d2,
-              fillColor = ~pal(v[[2]]),
-              stroke = FALSE,
-              fillOpacity = 1,
-              label = ~v[[2]],
-              highlightOptions = highlightOptions(
-                color = "white",
-                weight = 4,
-                fillColor = parametros$color,
-                bringToFront = TRUE
-              )
+  addPolygons(
+    data = d2,
+    fillColor = ~ pal(v[[2]]),
+    stroke = FALSE,
+    fillOpacity = 1,
+    label = ~ v[[2]],
+    highlightOptions = highlightOptions(
+      color = "white",
+      weight = 4,
+      fillColor = parametros$color,
+      bringToFront = TRUE
+    )
+  ) |>
+  addLegend(
+    position  = "topright",
+    na.label = "No disponible",
+    pal       = pal,
+    values    = v[[2]],
+    layerId   = "colorLegend"
   )
